3b329868a69eea63f976649fdfb1c1b0b4492851
unknown <anwarhosen575@gmail.com>
2023-02-12 07:20:28 +0600

    mouseenter-leave

--- events-jquery/js/main.js
+++ events-jquery/js/main.js
@@ -1,4 +1,33 @@
-function func1()
-{
-    document.getElementById("img1").style.width="500px";
-}
+// function func1()
+// {
+//     document.getElementById("img1").style.width="500px";
+// }
+
+// $("button").click(func1);
+// function func1(){
+//     $("#img1").css('width','500px');
+// }
+
+// $("button").click(function (){
+//     $("#img1").css('width','500px');
+// });
+
+// $("document").ready(function(){
+
+//     $("button").click(function (){
+//     $("#img1").css('width','500px');
+
+//     });
+// });
+
+$("document").ready(function(){
+
+    $("#img1").mouseenter(function (){
+    $("#img1").css('width','500px');
+
+    });
+    $("#img1").mouseleave(function (){
+        $("#img1").css('width','200px');
+    
+        });
+});
