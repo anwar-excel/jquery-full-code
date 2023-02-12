1360fdc2ee807315579b88468c051d279cb12841
unknown <anwarhosen575@gmail.com>
2023-02-12 07:01:22 +0600

    onclick

--- day-2/js/main.js
+++ day-2/js/main.js
@@ -33,10 +33,34 @@
 //    alert("double click");
 //     });
 // });
+// $(document).ready(function(){
+//    $('.one').hover(function(){
+//     alert('enter');
+//    },function(){
+//     alert('leave');
+//    });
+// });
+// $(document).ready(function(){
+//     $('button').on('click',function(){
+// alert("Clicked");
+//     });
+//  });
 $(document).ready(function(){
-   $('.one').hover(function(){
-    alert('enter');
-   },function(){
-    alert('leave');
-   });
-});
+    $('button').on({
+        click:function() {
+            console.log("click")
+        },
+        mouseenter:function() {
+            console.log("mouseenter")
+        },
+        mouseleave:function() {
+            console.log("mouseleave")
+        },
+        mousedown:function() {
+            console.log("mousedown")
+        },
+        mouseup:function() {
+            console.log("mouseup")
+        },
+    });
+ });
---
+++ events-jquery/js/main.js
@@ -0,0 +1,4 @@
+function func1()
+{
+    document.getElementById("img1").style.width="500px";
+}
