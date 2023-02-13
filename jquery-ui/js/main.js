b2b653e57c5cf54285ccf14148e7bf208a0749e8
unknown <anwarhosen575@gmail.com>
2023-02-13 09:04:01 +0600

    accordion-animation

--- jquery-ui/js/main.js
+++ jquery-ui/js/main.js
@@ -20,3 +20,12 @@ $("document").ready(function(){
         hide:{effect:"explode",duration:2000}
     });
 })
+
+$("document").ready(function(){
+    $("#div1").accordion({
+        collapsible: true,
+        icons:{header:"ui-icon-caret-1-s",activeHeader:"ui-icon-caret-1-n"},
+        animate:1000,
+        event:"mouseover"
+    });
+})
